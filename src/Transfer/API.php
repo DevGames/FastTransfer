7e1e628e3e7ea9d596459d64c9b47bda57e465ea
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-10-10 04:17:24 -0700

    Update API.php

--- src/Transfer/API.php
+++ src/Transfer/API.php
@@ -20,17 +20,20 @@ implements ABI
      
         if($this->mode == 1){
         
+            array_map(
+        function ($r1,$r2){
+        if($r1 == $r2){
+            unset($r1,$r2);
+            return;
+        }
+      }, array($ipserver),array($portserver));
+            
         list($a1,$a2) = array($ipserver,$portserver);
         
         $this->address = $a1;
         $this->port = $a2;
 
-        array_map(
-        function ($r1,$r2){
-        if($r1 == $r2){
-            unset($r1,$r2);
-        }
-      }, array($a1),array($a2));
+       
         $p->dataPacket($this);
         }
 
